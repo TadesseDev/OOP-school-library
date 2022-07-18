32362f2ff6488dd841e08398d5878276b317e69e
Tadesse Alemayehu <tadalem26@gmail.com>
2022-07-18 23:46:53 +0300

    Fix: lintter issues

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name,
             @age = age,
