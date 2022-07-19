503dda4a53739a7ae85c53b0bd4d7c52d22555ae
Tadesse-Alemayehu <Tadesse Alemayehu>
2022-07-19 21:49:01 +0300

    Fix: capitalize decoretor issues

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,7 +1,12 @@
 require_relative 'base_decorator'
 
+require_relative 'person'
 class CapitalizeDecorator < BaseDecorator
   def correct_name
-    super().capitalize
+    super().capitalize!
   end
 end
+
+person = Person.new(22, 'maximilianus')
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name
--- person.rb
+++ person.rb
@@ -4,14 +4,16 @@ require_relative 'nameable'
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
-def correct_name
-  @name
-end
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..100)
-    @name = name,
-            @age = age,
-            @parent_permission = parent_permission
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
