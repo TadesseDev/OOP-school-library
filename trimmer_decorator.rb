605a6e1c2b15bc29df3af2df963bb7778f0dc637
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-19 19:41:21 +0300

    Fix: linter issues

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,15 +1,18 @@
 require_relative 'person'
+
 require_relative 'base_decorator'
+
 require_relative 'capitalize_decorator'
+
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    super().
+    super().slice(0, 10)
   end
 end
 
 person = Person.new(22, 'maximilianus')
 puts person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(person)
-puts capitalizedTrimmedPerson.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(person)
+puts capitalized_trimmed_person.correct_name
