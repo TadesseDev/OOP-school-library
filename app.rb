0bd22fde2771443f46bf95ea3d0959e1efb39fcf
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-07-21 16:26:01 +0300

    Update: add and display books

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require_relative 'classroom'
 require_relative 'student'
 require_relative 'teacher'
-@class_room_1=Classroom.new('microverse_one')
-@persons=[]
+require_relative 'book'
+@class_room_1 = Classroom.new('microverse_one')
+@persons = []
+@books=[]
 def list_all_books
-  puts 'listing all books'
+  @books.each {|book| puts "Title: \"#{book.title}\", Author: \"#{book.author}\""}
 end
 
 def list_all_people
@@ -13,33 +15,39 @@ end
 
 def create_person
   print 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
-    input=gets.chop.to_i
-if(input==1||input==2)
-      print "age:"
-    age=gets.chomp.to_i
-    print "Name:"
-    name=gets.chomp
-  if input==1
-    print "Hase parent permission [Y/N]:"
-    permission=gets.chomp
-    permission=permission.upcase=='Y'?true:false
-    student=Student.new(age, @class_room_1, name, permission)
-    @persons.push(student)
-    puts "persons #{@persons.length}"
-  elsif input==2
-    print "specialization:"
-    specialization=gets.chomp
-    teacher=Teacher.new(age, specialization, name)
-    @persons.push(teacher)
-    puts "persons #{@persons.length}"
+  input = gets.chop.to_i
+  if [1, 2].include?(input)
+    print 'age:'
+    age = gets.chomp.to_i
+    print 'Name:'
+    name = gets.chomp
+    case input
+    when 1
+      print 'Hase parent permission [Y/N]:'
+      permission = gets.chomp
+      permission = permission.upcase == 'Y'
+      student = Student.new(age, @class_room_1, name, permission)
+      @persons.push(student)
+      puts "persons #{@persons.length}"
+    when 2
+      puts 'Person created  successfully'
+      specialization = gets.chomp
+      teacher = Teacher.new(age, specialization, name)
+      @persons.push(teacher)
+      puts 'Person created  successfully'
+    end
+  else
+    puts 'Invalid input'
   end
-else
-    puts "Invalid input"
-end
 end
 
 def create_book
-  puts 'create book'
+    print "Title: "
+    title=gets.chomp
+    print "Author: "
+    author=gets.chomp
+    @books.push(Book.new(title,author))
+    puts "Book created successfuly"
 end
 
 def create_rental
--- main.rb
+++ main.rb
@@ -1,34 +1,34 @@
 require_relative 'app'
 def main
-  options=[
-    "Pleas choose an option by entering a bunber",
-    "1 - List all books",
-    "2 - List all people",
-    "3 - create a person",
-    "4 - create a book",
-    "5 - create a rental",
-    "6 - List all rental for a given person id",
-    "7 - Exit"
+  options = [
+    'Pleas choose an option by entering a bunber',
+    '1 - List all books',
+    '2 - List all people',
+    '3 - create a person',
+    '4 - create a book',
+    '5 - create a rental',
+    '6 - List all rental for a given person id',
+    '7 - Exit'
   ]
   puts options
-  input=gets.chomp.to_i
+  input = gets.chomp.to_i
   while input != 7
     case input
-      when 1
+    when 1
       list_all_books
-      when 2
+    when 2
       list_all_people
-      when 3
+    when 3
       puts create_person
-      when 4
+    when 4
       puts create_book
-      when 5
+    when 5
       puts create_rental
-      when 6
+    when 6
       puts list_all_rentals
     end
-  puts options
-  input=gets.chomp.to_i
+    puts options
+    input = gets.chomp.to_i
   end
 end
 
