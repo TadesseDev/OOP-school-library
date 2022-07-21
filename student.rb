9b2830248b34ce893f219b424c78bd857358f05f
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-07-21 08:11:08 +0300

    Update: student class

--- student.rb
+++ student.rb
@@ -2,6 +2,7 @@ require_relative 'person'
 
 # This a Student class
 class Student < Person
+  attr_writer :classroom
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -9,6 +10,10 @@ class Student < Person
 
   def play_hooky
     '"¯\(ツ)/¯"'
+  end
+  def classroom(classroom)
+    @classroom = classroom
+    classroom.students(self) unless classroom.students.include?(self)
   end
 end
 
