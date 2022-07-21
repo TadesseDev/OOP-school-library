e3117468049f84ad2b15717d63ec97c9fd24bd98
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-07-21 15:19:35 +0300

    Create: initial app methods

---
+++ app.rb
@@ -0,0 +1,23 @@
+def list_all_books
+  puts 'listing all books'
+end
+
+def list_all_people
+  puts 'listing all people'
+end
+
+def create_person
+  puts 'create person'
+end
+
+def create_book
+  puts 'create book'
+end
+
+def create_rental
+  puts 'create rentals'
+end
+
+def list_all_rentals
+  puts 'list all rents'
+end
