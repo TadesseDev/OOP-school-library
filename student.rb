c072b05b1ec7a19844afc95358397e475d67410b
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-07-25 15:36:01 +0300

    Restore: parent permission for student SINGLE RESPONSIBLITY

--- student.rb
+++ student.rb
@@ -5,15 +5,29 @@ class Student < Person
   attr_accessor :classroom
 
   # rubocop:disable Style/OptionalBooleanParameter
-  def initialize(age, classroom, name = 'Unknown')
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
     # rubocop:enable Style/OptionalBooleanParameter
     super(age, name)
     @classroom = classroom
+    @parent_permission = parent_permission
   end
 
+  def can_use_services?
+    if isof_age? || @parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  private
+  def isof_age?
+    @age >= 18
+  end
   # setter
   def update_classroom(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
+
