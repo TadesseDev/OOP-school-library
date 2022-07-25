9ab84446ee28419c519a54943dfe3de5930910a5
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-07-25 15:33:26 +0300

    Remove: classeoom uneccessary feature KISS

--- classroom.rb
+++ classroom.rb
@@ -1,13 +1,7 @@
 class Classroom
   attr_accessor :label, :students
-
   def initialize(label)
     @label = label
     @students = []
-  end
-
-  def add_students=(student)
-    @students.push(student)
-    student.update_classroom(self)
   end
 end
