d5e20f054e1bb2a39aa011c22ab9830f7a57be4e
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-27 23:04:20 +0300

    Handle: exceptionwith file.exist

--- proccess_file.rb
+++ proccess_file.rb
@@ -12,6 +12,7 @@ class ProccessJsonFile
   end
 
   def read_json(options: {})
+    return []  unless File.exists?(@file_location)
     file = File.new(@file_location, 'r')
     file_data = JSON.parse(file.read, options)
     file.close
