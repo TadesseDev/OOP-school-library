cff7453a5bf790b7a8754e008dae5575129b6577
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-28 03:01:31 +0300

    Fix: linter issue

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,16 +3,16 @@ require_relative '../book'
 describe Book do
   context 'Book class saves all valid values' do
     before(:each) do
-      @Book = Book.new('Book Title', 'Book Author')
+      @book = Book.new('Book Title', 'Book Author')
     end
     it 'saves and return valid title and author' do
-      expect(@Book.title).to eql 'Book Title'
-      expect(@Book.author).to eql 'Book Author'
+      expect(@book.title).to eql 'Book Title'
+      expect(@book.author).to eql 'Book Author'
     end
 
     it 'have valid data type' do
-      expect(@Book.title).to be_an_instance_of(String)
-      expect(@Book.author).to be_an_instance_of(String)
+      expect(@book.title).to be_an_instance_of(String)
+      expect(@book.author).to be_an_instance_of(String)
     end
   end
 end
